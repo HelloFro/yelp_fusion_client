7b7338910bf9f319f754eb8f53a07acf11596db9
Fro Warner <hellofroenn@gmail.com>
2021-03-08 14:50:41 -0800

    Update category.dart

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Gives details for each Category alias and title.
 class Category {
   /** Alias of a category. When searching for business in certain categories, use alias rather than the title. */
@@ -6,23 +8,31 @@ class Category {
   /// Title of a category for display purpose.
   String title;
 
-  Category({this.alias, this.title});
+  Category({
+    this.alias,
+    this.title,
+  });
 
-  factory Category.fromJson(Map<String, dynamic> data) {
-    return Category(alias: data['alias'], title: data['title']);
-  }
-}
-
-/// A list of category title and alias pairs associated with a business.
-class Categories {
-  List<Category> categories;
-
-  Categories({this.categories});
-
-  factory Categories.fromList(List<dynamic> categoryList) {
-    return Categories(
-      categories:
-          categoryList.map((e) => Category.fromJson(e['categories'])).toList(),
+  factory Category.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Category(
+      alias: map['alias'],
+      title: map['title'],
     );
+  }
+
+  factory Category.fromJson(String source) => Category.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Category(alias: $alias, title: $title)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Category &&
+      o.alias == alias &&
+      o.title == title;
   }
 }
