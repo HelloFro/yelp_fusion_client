b7d631831226e72f44e04fdba14a19be3b8ae98c
Fro Warner <hellofroenn@gmail.com>
2021-03-08 14:18:29 -0800

    Create messaging.dart

---
+++ lib/models/messaging.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+/// Contains Business Messaging / Request a Quote information for a business.
+class Messaging {
+  /// Action Link URL that drops user directly in to the messaging flow for a business.
+  final String url;
+
+  /** Indicates what kind of messaging can be done with a business, ie. 
+    * "Request a Quote" for a home services business, 
+    * or "Request a Consultation" for a legal services business. 
+    * Note: Text will be localized based on the "locale" input parameter. */
+  final String useCaseText;
+
+  Messaging({
+    this.url,
+    this.useCaseText,
+  });
+
+  factory Messaging.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Messaging(
+      url: map['url'],
+      useCaseText: map['use_case_text'],
+    );
+  }
+
+  factory Messaging.fromJson(String source) => Messaging.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Messaging(url: $url, useCaseText: $useCaseText)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Messaging &&
+      o.url == url &&
+      o.useCaseText == useCaseText;
+  }
+}
