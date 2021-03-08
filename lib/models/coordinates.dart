cc6847f787348f0934274e9368291c12fc1bc34a
Fro Warner <hellofroenn@gmail.com>
2021-03-08 14:13:15 -0800

    Create coordinates.dart

---
+++ lib/models/coordinates.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+/// The coordinates of a business.
+class Coordinates {
+  /// The latitude of a business.
+  final double latitude;
+
+  /// The longitude of a business.
+  final double longitude;
+
+  Coordinates({
+    this.latitude,
+    this.longitude,
+  });
+
+  factory Coordinates.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+  
+    return Coordinates(
+      latitude: map['latitude'],
+      longitude: map['longitude'],
+    );
+  }
+
+  factory Coordinates.fromJson(String source) => Coordinates.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Coordinates(latitude: $latitude, longitude: $longitude)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+  
+    return o is Coordinates &&
+      o.latitude == latitude &&
+      o.longitude == longitude;
+  }
+}
