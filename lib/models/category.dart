ec4a405ffc9cf5162ca250c74cbc4ec11f382323
Fro Warner <froennkastle@gmail.com>
2021-03-08 13:14:56 -0800

    Create category.dart

---
+++ lib/models/category.dart
@@ -0,0 +1,28 @@
+/* Gives details for each Category alias and title. */
+class Category {
+  /* Alias of a category, when searching for business in certain categories, use alias rather than the title. */
+  String alias;
+
+  /* Title of a category for display purpose. */
+  String title;
+
+  Category({this.alias, this.title});
+
+  factory Category.fromJson(Map<String, dynamic> data) {
+    return Category(alias: data['alias'], title: data['title']);
+  }
+}
+
+/* A list of category title and alias pairs associated with a business. */
+class Categories {
+  List<Category> categories;
+
+  Categories({this.categories});
+
+  factory Categories.fromList(List<dynamic> categoryList) {
+    return Categories(
+      categories:
+          categoryList.map((e) => Category.fromJson(e['categories'])).toList(),
+    );
+  }
+}
