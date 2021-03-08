3ada210ab03b0c603bf1f3c6b697ef960fb5d886
Fro Warner <froennkastle@gmail.com>
2021-03-08 12:54:51 -0800

    Update yelp_fusion_client.dart
    - import necessary packages

--- lib/yelp_fusion_client.dart
+++ lib/yelp_fusion_client.dart
@@ -1,5 +1,12 @@
 library yelp_fusion_client;
 
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
+
+
 /* A Dart class to get all the endpoints of the Yelp Fusion API. */
 class YelpFusion {
   /* Your Yelp Fusion API Key */
