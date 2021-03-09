a12373c0e418889e309ad93ee1a57d508ae0285a
Fro Warner <hellofroenn@gmail.com>
2021-03-08 22:33:28 -0800

    Update yelp_fusion_client.dart

--- lib/yelp_fusion_client.dart
+++ lib/yelp_fusion_client.dart
@@ -4,15 +4,41 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
+import 'package:yelp_fusion_client/models/business_endpoints/business_details.dart';
 
-
-
-/* A Dart class to get all the endpoints of the Yelp Fusion API. */
+/// A Dart class to get all the endpoints of the Yelp Fusion API.
 class YelpFusion {
-  /* Your Yelp Fusion API Key */
+  /// Yelp Fusion API Key
   String apiKey;
 
-  YelpFusion({@required this.apiKey}) : assert(apiKey != null);
+  Map<String, String> _headers;
+
+  /// Requires API Key, sets GET request headers
+  YelpFusion({@required this.apiKey}) : assert(apiKey != null) {
+    _headers = {
+      'Authorization': 'Bearer $apiKey',
+      "Content-type": "application/json",
+    };
+  }
+
+  /// Get rich business data, such as name, address, phone number, photos, Yelp rating, price levels and hours of operation
+  Future fetchBusinessDetails(
+    {@required String id,
+    bool asObject = true}
+  ) async {
+    assert(id != null);
+
+    var url = Uri.https('api.yelp.com', 'v3/businesses/$id');
+
+    final response = await http.get(url, headers: _headers);
+
+    Map<String, dynamic> jsonData = json.decode(response.body);
+
+    if(jsonData.containsKey('error'))
+      return jsonData['error'];
+    else
+      return asObject ? BusinessDetails.fromJson(response.body) : jsonData;
+  }
 
   
 }
