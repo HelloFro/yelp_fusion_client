3f82b130aa418d351102747f030875c4dc21a0a0
Fro Warner <hellofroenn@gmail.com>
2022-02-24 20:35:30 -0800

    Update yelp_fusion_client_test.dart
    + Grouped tests

--- test/yelp_fusion_client_test.dart
+++ test/yelp_fusion_client_test.dart
@@ -6,52 +6,53 @@ import 'env/env.dart';
 void main() {
   final api = YelpFusion(apiKey: Env.api_key);
 
-  test('Business Details ARE Fetched by Business alias/id', () async {
-    var details = await api
-      .fetchBusinessDetails(id: 'north-india-restaurant-san-francisco');
+  group('fetchBusinessDetails', () {
+    test('Business Details ARE Fetched by Business alias/id', () async {
+      var details = await api.fetchBusinessDetails(id: 'north-india-restaurant-san-francisco');
 
-    expect(details.alias, 'north-india-restaurant-san-francisco');
+      expect(details.alias, 'north-india-restaurant-san-francisco');
+    });
+
+    test('Business Details ARE NOT Fetched by Business alias/id and error code is returned', () async {
+      var details = await api.fetchBusinessDetails(id: 'DNE');
+
+      expect(details['code'], 'BUSINESS_NOT_FOUND');
+    });
   });
 
-  test('Business Details ARE NOT Fetched by Business alias/id and error code is returned', () async {
-    var details = await api
-      .fetchBusinessDetails(id: 'poop');
+  group('fetchAutocomplete', () {
+    test('Autocomplete IS Fetched by user text based on lat & long', () async {
+      var autocomplete = await api.fetchAutocomplete(text: 'del', latitude: 37.786882, longitude: -122.399972);
 
-    expect(details['code'], 'BUSINESS_NOT_FOUND');
+      expect(autocomplete.terms.text.first, 'Delivery');
+    });
   });
 
-  test('Autocomplete IS Fetched by user text based on lat & long', () async {
-    var autocomplete = await api
-      .fetchAutocomplete(text: 'del', latitude: 37.786882, longitude: -122.399972);
+  group('fetchBusinessSearch', () {
+    test('BusinessSearch IS Fetched by term based on lat & long AND first business MATCHES expected alias', () async {
+      var search = await api.fetchBusinessSearch(term: 'delis', latitude: 37.786882, longitude: -122.399972);
 
-    expect(autocomplete.terms.text.first, 'Delivery');
+      expect(search.businesses.businesses.first.alias, "molinari-delicatessen-san-francisco");
+    });
+
+    test('BusinessSearch IS Fetched by term based on lat & long AND returns at least one business', () async {
+      var search = await api.fetchBusinessSearch(term: 'delis', latitude: 37.786882, longitude: -122.399972);
+
+      expect(search.total, greaterThan(0));
+    });
   });
 
-  test('BusinessSearch IS Fetched by term based on lat & long AND first business MATCHES expected alias', () async {
-    var search = await api
-      .fetchBusinessSearch(term: 'delis', latitude: 37.786882, longitude: -122.399972);
+  group('fetchBusinessReviews', () {
+    test('Business Reviews ARE Fetched by Business alias/id AND returns at least one business review', () async {
+      var reviews = await api.fetchBusinessReviews(id: 'north-india-restaurant-san-francisco');
 
-    expect(search.businesses.businesses.first.alias, "molinari-delicatessen-san-francisco");
-  });
+      expect(reviews.total, greaterThan(0));
+    });
 
-  test('BusinessSearch IS Fetched by term based on lat & long AND returns at least one business', () async {
-    var search = await api
-      .fetchBusinessSearch(term: 'delis', latitude: 37.786882, longitude: -122.399972);
+    test('Business Reviews ARE Fetched by Business alias/id AND review list contains no more than three business reviews', () async {
+      var reviews = await api.fetchBusinessReviews(id: 'north-india-restaurant-san-francisco');
 
-    expect(search.total, greaterThan(0));
-  });
-
-  test('Business Reviews ARE Fetched by Business alias/id AND returns at least one business review', () async {
-    var reviews = await api
-      .fetchBusinessReviews(id: 'north-india-restaurant-san-francisco');
-
-    expect(reviews.total, greaterThan(0));
-  });
-
-  test('Business Reviews ARE Fetched by Business alias/id AND review list contains no more than three business reviews', () async {
-    var reviews = await api
-      .fetchBusinessReviews(id: 'north-india-restaurant-san-francisco');
-
-    expect(reviews.reviews.length, lessThanOrEqualTo(3));
+      expect(reviews.reviews.length, lessThanOrEqualTo(3));
+    });
   });
 }
